b49a6403eba1be27f1f02721d5f95eb6fa28ca2f
Marcus B-S <Birdman.m1jbs@gmail.com>
2016-03-20 18:29:04 -0500

    Start layout for pom

--- MapTracking/Online.Designer.cs
+++ MapTracking/Online.Designer.cs
@@ -31,13 +31,21 @@
             this.BaseSplit = new System.Windows.Forms.SplitContainer();
             this.tabSettings = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblAuthToken = new System.Windows.Forms.Label();
+            this.tbAuthToken = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.OpenLog = new System.Windows.Forms.OpenFileDialog();
+            this.findConfig = new System.Windows.Forms.Button();
+            this.tbClientLocation = new System.Windows.Forms.TextBox();
+            this.lbChars = new System.Windows.Forms.Label();
+            this.tbCharacter = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.BaseSplit)).BeginInit();
             this.BaseSplit.Panel1.SuspendLayout();
             this.BaseSplit.SuspendLayout();
             this.tabSettings.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BaseSplit
@@ -75,6 +83,47 @@
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.lblAuthToken, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbAuthToken, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.findConfig, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbClientLocation, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbChars, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tbCharacter, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 156F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(250, 241);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblAuthToken
+            // 
+            this.lblAuthToken.AutoSize = true;
+            this.lblAuthToken.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAuthToken.Location = new System.Drawing.Point(3, 0);
+            this.lblAuthToken.Name = "lblAuthToken";
+            this.lblAuthToken.Size = new System.Drawing.Size(85, 17);
+            this.lblAuthToken.TabIndex = 0;
+            this.lblAuthToken.Text = "Auth Token:";
+            // 
+            // tbAuthToken
+            // 
+            this.tbAuthToken.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbAuthToken.Location = new System.Drawing.Point(128, 3);
+            this.tbAuthToken.Name = "tbAuthToken";
+            this.tbAuthToken.PasswordChar = '*';
+            this.tbAuthToken.Size = new System.Drawing.Size(119, 20);
+            this.tbAuthToken.TabIndex = 1;
+            // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
@@ -85,19 +134,47 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // tableLayoutPanel1
+            // OpenLog
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(250, 241);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.OpenLog.FileName = "Client.txt";
+            // 
+            // findConfig
+            // 
+            this.findConfig.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.findConfig.Location = new System.Drawing.Point(128, 30);
+            this.findConfig.Name = "findConfig";
+            this.findConfig.Size = new System.Drawing.Size(119, 21);
+            this.findConfig.TabIndex = 2;
+            this.findConfig.Text = "Find Config";
+            this.findConfig.UseVisualStyleBackColor = true;
+            this.findConfig.Click += new System.EventHandler(this.findConfig_Click);
+            // 
+            // tbClientLocation
+            // 
+            this.tbClientLocation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbClientLocation.Location = new System.Drawing.Point(3, 30);
+            this.tbClientLocation.Name = "tbClientLocation";
+            this.tbClientLocation.ReadOnly = true;
+            this.tbClientLocation.Size = new System.Drawing.Size(119, 20);
+            this.tbClientLocation.TabIndex = 3;
+            // 
+            // lbChars
+            // 
+            this.lbChars.AutoSize = true;
+            this.lbChars.Location = new System.Drawing.Point(3, 54);
+            this.lbChars.Name = "lbChars";
+            this.lbChars.Size = new System.Drawing.Size(110, 13);
+            this.lbChars.TabIndex = 4;
+            this.lbChars.Text = "character one per line";
+            // 
+            // tbCharacter
+            // 
+            this.tbCharacter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbCharacter.Location = new System.Drawing.Point(128, 57);
+            this.tbCharacter.Multiline = true;
+            this.tbCharacter.Name = "tbCharacter";
+            this.tbCharacter.Size = new System.Drawing.Size(119, 150);
+            this.tbCharacter.TabIndex = 5;
             // 
             // Online
             // 
@@ -112,6 +189,8 @@
             this.BaseSplit.ResumeLayout(false);
             this.tabSettings.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -123,5 +202,12 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblAuthToken;
+        private System.Windows.Forms.TextBox tbAuthToken;
+        private System.Windows.Forms.OpenFileDialog OpenLog;
+        private System.Windows.Forms.Button findConfig;
+        private System.Windows.Forms.TextBox tbClientLocation;
+        private System.Windows.Forms.Label lbChars;
+        private System.Windows.Forms.TextBox tbCharacter;
     }
 }
--- MapTracking/Online.cs
+++ MapTracking/Online.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace poeMapTracking
 {
     public partial class Online : Form
     {
+        FileSystemWatcher logWatch;
         public Online()
         {
+            logWatch = new FileSystemWatcher();
             InitializeComponent();
+        }
+
+        private void findConfig_Click(object sender, EventArgs e)
+        {
+            this.OpenLog.Multiselect = false;
+            DialogResult dr =  this.OpenLog.ShowDialog();
+            if (this.OpenLog.FileName != null || this.OpenLog.FileName != "")
+            {
+                this.tbClientLocation.Text = this.OpenLog.FileName;
+                FileInfo file = new FileInfo(this.tbClientLocation.Text);
+                logWatch.Path = file.Directory.FullName;
+                logWatch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+           | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                logWatch.Filter = file.Name;
+                //logWatch.Changed += new FileSystemEventArgs(OnChange);
+            }
+        }//end findcfg
+        private static void OnChange(object source, FileSystemEventArgs e)
+        { 
+            //StreamReader sr = new StreamReader(e.FullPath);
+            //string[] file = sr.ReadToEnd().Split(new string[]{Environment.NewLine},StringSplitOptions.None);
         }
     }
 }
